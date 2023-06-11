bf433b15c48d3fb889efe0972a3d6c3c1f8c0f67
TiieuTiien <andfjrj123@gmail.com>
2023-06-11 09:29:10 +0700

    11/0/2023 DES first commited

--- PhamQuocTien/DES/src/model/DESModel.java
+++ PhamQuocTien/DES/src/model/DESModel.java
@@ -1,5 +1,25 @@
 package model;
 
 public class DESModel {
+	public static void main(String[] args) {
+        String keyString = "MyKey123"; // Example key string
 
+        // Convert the key string to bytes using ASCII encoding
+        byte[] keyBytes = keyString.getBytes();
+
+        // Create a 64-bit key using the first 8 bytes
+        long encryptionKey = bytesToLong(keyBytes, 0, 8);
+
+        System.out.println("Encryption Key: " + encryptionKey);
+    }
+
+    // Helper method to convert a byte array to a long value
+    private static long bytesToLong(byte[] bytes, int offset, int length) {
+        long result = 0;
+        for (int i = offset; i < offset + length; i++) {
+            result <<= 8; // Shift the existing bits to the left
+            result |= (bytes[i] & 0xFF); // Set the least significant byte
+        }
+        return result;
+    }
 }
