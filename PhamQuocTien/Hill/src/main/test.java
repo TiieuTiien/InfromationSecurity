6f61de30ad1f047db139612a76c6f705d759ff1a
TiieuTiien <andfjrj123@gmail.com>
2023-06-18 12:17:16 +0700

    School commited

---
+++ PhamQuocTien/Hill/src/main/test.java
@@ -0,0 +1,13 @@
+package main;
+
+import model.HillModel;
+
+public class test {
+	public static void main(String[] args) {
+		HillModel hillModel = new HillModel();
+		
+		System.out.println(hillModel.findInverse(26, 5));
+		
+		
+	}
+}
