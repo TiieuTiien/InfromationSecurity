3ac11318292548ce75e51a597b63cba7aa4964da
TiieuTiien <andfjrj123@gmail.com>
2023-06-18 16:01:26 +0700

    18/06/2023 AES.js commited

--- FinalProject/AES.js
+++ FinalProject/AES.js
@@ -1,46 +1,64 @@
-// AES.js
+const crypto = require('crypto');
 
 // Function to generate a random encryption key
 function generateKey() {
-    return crypto.getRandomValues(new Uint8Array(32)); // 32 bytes = 256 bits key size
+    return crypto.randomBytes(32); // 32 bytes = 256 bits key size
 }
 
-// Function to encrypt plaintext
-async function encrypt(plaintext, key) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(plaintext);
-    const cryptoKey = await crypto.subtle.importKey('raw', key, 'AES-CBC', true, ['encrypt']);
-    const iv = crypto.getRandomValues(new Uint8Array(16));
-    const encryptedData = await crypto.subtle.encrypt(
-        {
-            name: 'AES-CBC',
-            iv: iv,
-        },
-        cryptoKey,
-        data
-    );
-    const encryptedBytes = new Uint8Array(encryptedData);
-    const encryptedHex = Array.prototype.map.call(encryptedBytes, (byte) => ('00' + byte.toString(16)).slice(-2)).join('');
-    return {
-        iv: Array.prototype.map.call(iv, (byte) => ('00' + byte.toString(16)).slice(-2)).join(''),
-        encryptedData: encryptedHex,
-    };
+// Function to generate round keys based on AES key schedule
+function generateRoundKeys(key) {
+    const roundKeys = [];
+    const cipher = crypto.createCipheriv('aes-256-ecb', key, null);
+    const zeroBlock = Buffer.alloc(16);
+
+    let currKey = Buffer.from(key);
+    for (let i = 0; i < 14; i++) {
+        currKey = cipher.update(currKey);
+        roundKeys.push(currKey);
+    }
+
+    return roundKeys;
 }
 
-// Function to decrypt ciphertext
-async function decrypt(ciphertext, key, iv) {
-    const cryptoKey = await crypto.subtle.importKey('raw',key,'AES-CBC',true,['decrypt']);
-    const ciphertextBytes = new Uint8Array(ciphertext.match(/.{1,2}/g).map((byte) => parseInt(byte, 16)));
-    const decryptedData = await crypto.subtle.decrypt(
-        {
-            name: 'AES-CBC',
-            iv: new Uint8Array(
-                iv.match(/.{1,2}/g).map((byte) => parseInt(byte, 16))
-            ),
-        },
-        cryptoKey,
-        ciphertextBytes
-    );
-    const decoder = new TextDecoder();
-    return decoder.decode(decryptedData);
+// Function to perform AES encryption with 14 rounds in AES-ECB mode
+function encryptAES(plaintext, key) {
+    const roundKeys = generateRoundKeys(key);
+    let ciphertext = Buffer.from(plaintext);
+
+    for (let i = 0; i < 13; i++) {
+        const cipher = crypto.createCipheriv('aes-256-ecb', roundKeys[i], null);
+        ciphertext = Buffer.concat([cipher.update(ciphertext), cipher.final()]);
+    }
+
+    const finalCipher = crypto.createCipheriv('aes-256-ecb', roundKeys[13], null);
+    const finalCiphertext = Buffer.concat([finalCipher.update(ciphertext), finalCipher.final()]);
+
+    return finalCiphertext;
 }
+
+// Function to perform AES decryption with 14 rounds in AES-ECB mode
+function decryptAES(ciphertext, key) {
+    const roundKeys = generateRoundKeys(key);
+    let decryptedText = Buffer.from(ciphertext);
+
+    const finalDecipher = crypto.createDecipheriv('aes-256-ecb', roundKeys[13], null);
+    decryptedText = Buffer.concat([finalDecipher.update(decryptedText), finalDecipher.final()]);
+
+    for (let i = 12; i >= 0; i--) {
+        const decipher = crypto.createDecipheriv('aes-256-ecb', roundKeys[i], null);
+        decryptedText = Buffer.concat([decipher.update(decryptedText), decipher.final()]);
+    }
+
+    return decryptedText;
+}
+
+const plaintext = 'Hello, World!';
+const key = generateKey();
+
+console.log('Plaintext:', plaintext);
+
+const ciphertext = encryptAES(plaintext, key);
+console.log('Ciphertext:', ciphertext.toString('base64'));
+
+const decryptedText = decryptAES(ciphertext, key);
+console.log('Decrypted Text:', decryptedText.toString('utf8'));
